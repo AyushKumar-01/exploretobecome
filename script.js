b9b34615543c8ae4db3928f78b5e305bfa0e032c
Ayush Kumar <136918365+AyushKumar-01@users.noreply.github.com>
2023-07-05 20:10:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Ghatiya logic for showing that form has been submitted(only for show-off purpose)-->
+
+document.getElementById('btn').addEventListener('click', function () {
+    document.getElementById('form').style.display = "none";
+    document.getElementById('btn').style.display = "none";
+    document.getElementById('jsimg').style.display = "flex";
+    document.getElementById('distext').style.display = "flex";
+})
